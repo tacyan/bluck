ad3c789a1b2e1685a0f2399c08ecfd9146009591
tacyan <varuvaru10000@yahoo.co.jp>
2017-09-18 20:28:57 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  'how are you?'
+end
+
+get '/path/to' do
+  "this is [/path/to]"
+end
+
+get '/hello/*' do |name|
+  "hello #{name}. how are you?"
+end
+
+get '/erb_template_page' do
+  erb :erb_template_page
+end
